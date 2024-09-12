a359591ba633fb257fb164908e31858bcf36a71f
Dusan Figala <dusan@oxidian.com>
2024-09-12 16:28:54 +0200

    Modify cwl tool with docker req

--- eoap_gen/cwl.py
+++ eoap_gen/cwl.py
@@ -1,6 +1,12 @@
 import os
 from pathlib import PurePath, Path
 import subprocess
+from pathlib import Path, PurePath
+
+from cwl_utils.parser import CommandLineTool, load_document_by_uri, save
+from cwl_utils.parser.cwl_v1_0 import DockerRequirement
+from ruamel.yaml import YAML
+
 from eoap_gen.template import get_template
 
 
@@ -28,3 +34,20 @@ def generate_cwl_cli(
     subprocess.run(cmd, shell=True, executable="/bin/bash")
 
 
+def modify_cwl_cli(cwl_path: os.PathLike, docker_url: str):
+    if not cwl_path:
+        raise ValueError("Invalid path to cwl file.")
+    if not docker_url:
+        raise ValueError("Invalid docker image url.")
+
+    tool_obj: CommandLineTool = load_document_by_uri(cwl_path)
+
+    tool_obj.hints = [DockerRequirement(dockerPull=docker_url)]
+    tool_obj.baseCommand = ["python", "/app/app.py"]
+
+    tool_dict = save(tool_obj)
+    yaml = YAML()
+    yaml.default_flow_style = False
+
+    with open(cwl_path, "w") as f:
+        yaml.dump(tool_dict, f)

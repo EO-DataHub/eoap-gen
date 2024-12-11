da5fa2d20b149e843f844626bea6993b08a57ed4
Dusan Figala <dusanfigala@protonmail.com>
2024-12-11 13:15:56 +0100

    Run micromamba container entrypoint

--- eoap_gen/cwl.py
+++ eoap_gen/cwl.py
@@ -132,7 +132,12 @@ def modify_cwl_cli(cwl_path: Path, docker_url: str, step: StepConfig):
         DockerRequirement(dockerPull=docker_url),
         InlineJavascriptRequirement(),
     ]
+
     tool_obj.baseCommand = ["python", "/app/app.py"]
+    if step.conda:
+        # if using micromamba, need to run the entrypoint script explicitly
+        tool_obj.baseCommand.insert(0, "/usr/local/bin/_entrypoint.sh")
+
     for inp in step.inputs:
         if inp.type_:
             inp_config = next(
